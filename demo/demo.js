586b0f94ef4a538b0c581797a4e55c336dbc68a8
shahvrushali22 <shahvrushali22@gmail.com>
2019-08-20 00:21:49 +0530

    Added all the files

---
+++ demo/demo.js
@@ -0,0 +1,18 @@
+/* =================================
+===  EXPAND COLLAPSE            ====
+=================================== */
+ $(document).ready(function(){
+ $('#toggle-switcher').click(function(){
+		if($(this).hasClass('open')){
+			$(this).removeClass('open');
+			$('#switch-style').animate({'left':'-220px'});
+		}else{
+			$(this).addClass('open');
+			$('#switch-style').animate({'left':'0'});
+		}
+	});
+});
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(".projects").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 2000,
+    loop: true,
+    nav: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            autoplayHoverPause: true,
+        },
+    //     480:{
+    //         items: 2,
+    //     },
+    //     768:{
+    //         items: 3,
+    // },
+},
+});
